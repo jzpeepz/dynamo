1c31edd5843c2d55dec6a3bb50964f17a10295fa
Jonathan Peoples <jonathan.peoples@gmail.com>
2018-01-22 12:21:34 -0600

    Moving from lists function to pluck

--- src/resources/views/partials/fields/hasMany.blade.php
+++ src/resources/views/partials/fields/hasMany.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="" title="Position: {{ $field->position }}">{{ $field->label }}</label>
-    {!! Form::select($field->key.'[]', $field->options['options'], $item->{$field->key}->lists('id')->toArray(), ['multiple' => true, 'class' => 'form-control '.$field->getOption('class')]) !!}
+    {!! Form::select($field->key.'[]', $field->options['options'], $item->{$field->key}->pluck('id')->toArray(), ['multiple' => true, 'class' => 'form-control '.$field->getOption('class')]) !!}
     @if (! empty($field->getOption('help')))
         <p class="help-block">{!! $field->getOption('help') !!}</p>
     @endif
--- src/resources/views/stubs/partials/fields/hasMany.php
+++ src/resources/views/stubs/partials/fields/hasMany.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="" title="Position: <?= $field->position ?>"><?= $field->label ?></label>
-    {!! Form::select('<?= $field->key ?>[]', $dynamo->getField('<?= $field->key ?>')->getSelectOptions(), $item-><?= $field->key ?>->lists('id')->toArray(), ['multiple' => true, 'class' => 'form-control <?= $field->getOption('class') ?>']) !!}
+    {!! Form::select('<?= $field->key ?>[]', $dynamo->getField('<?= $field->key ?>')->getSelectOptions(), $item-><?= $field->key ?>->pluck('id')->toArray(), ['multiple' => true, 'class' => 'form-control <?= $field->getOption('class') ?>']) !!}
     <?php if (! empty($field->getOption('help'))): ?>
         <p class="help-block"><?= $field->getOption('help') ?></p>
     <?php endif; ?>
