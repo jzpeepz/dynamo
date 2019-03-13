4ba5ef7640f34c6176902658ff54dccd172f8aa8
Jonathan Peoples <jonathan.peoples@gmail.com>
2019-03-12 22:01:08 -0500

    Adding some documentation to config

--- src/config/dynamo.php
+++ src/config/dynamo.php
@@ -21,9 +21,36 @@ return [
 
     'default_has_many_class' => '',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Model use statements
+    |--------------------------------------------------------------------------
+    |
+    | This value contains an array of the classes that should be imported into
+    | the generated model class.
+    |
+    */
     'model_uses' => [],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Interfaces that the model implements
+    |--------------------------------------------------------------------------
+    |
+    | This value contains an array of the interfaces that should be
+    | implemented by the generated model class.
+    |
+    */
     'model_implements' => [],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Model traits
+    |--------------------------------------------------------------------------
+    |
+    | This value contains an array of the traits that should be used by the
+    | generated model class.
+    |
+    */
     'model_traits' => [],
 ];
