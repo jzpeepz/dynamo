9a9fdd645ab4555b95581d82ea18123e311afaea
Colton Williams <clwilliams4545@gmail.com>
2019-04-08 09:47:55 -0500

    doc updates 6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,9 @@ copyright = u'2018, Jonathan Peoples, Colton Williams'
 author = u'Jonathan Peoples, Colton Williams'
 
 # The short X.Y version
-version = u''
+version = '1.3.5'
 # The full version, including alpha/beta/rc tags
-release = u'1.2.8'
+release = '1.3.5'
 
 
 # -- General configuration ---------------------------------------------------
