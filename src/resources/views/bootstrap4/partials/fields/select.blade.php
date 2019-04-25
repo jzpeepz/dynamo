3641ed28cf8391e98413428fe66eda7887eb0c50
Jonathan Peoples <jonathan.peoples@gmail.com>
2019-04-25 11:31:41 -0500

    Updating select to handle multiple attribute properly

--- src/resources/views/bootstrap4/partials/fields/select.blade.php
+++ src/resources/views/bootstrap4/partials/fields/select.blade.php
@@ -1,12 +1,16 @@
 @if ($display)
 
+    <?php
+    $multiple = ! empty($field->getOption('multiple'));
+    ?>
+
     <div class="form-group">
         <label for="" title="Position: {{ $field->position }}">{{ $field->label }}</label>
         @if (! empty($field->getOption('tooltip')))
             <i style="font-size: 17px; padding-left: 2px;" class="fas fa-question-circle" data-toggle="tooltip" data-html="true"
                 title="{!! $field->getOption('tooltip') !!}"></i>
         @endif
-        {!! Form::select($field->key, $field->getSelectOptions(), $item->{$field->key}, ['class' => 'form-control '.$field->getOption('class')]) !!}
+        {!! Form::select($field->key . ($multiple ? '[]' : ''), $field->getSelectOptions(), $item->{$field->key}, ['class' => 'form-control '.$field->getOption('class'), 'multiple' => $multiple]) !!}
         @if (! empty($field->getOption('help')))
             <p class="help-block">{!! $field->getOption('help') !!}</p>
         @endif
