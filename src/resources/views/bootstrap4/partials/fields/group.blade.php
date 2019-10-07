0ff3c5fdc8a7357282b9e00dd0d9b768f6fd0de1
Colton Williams <clwilliams4545@gmail.com>
2019-10-07 11:12:01 -0500

    bug fix on tooltip for groups

--- src/resources/views/bootstrap4/partials/fields/group.blade.php
+++ src/resources/views/bootstrap4/partials/fields/group.blade.php
@@ -5,10 +5,6 @@ $group = $dynamo->getGroup($field->key);
 {!! $group->renderBefore() !!}
 
 <div class="dynamo-group dynamo-group-{{ $group->name }} {{ $group->options->get('class') }}">
-    @if (! empty($field->getOption('tooltip')))
-        <i id="dont-show-on-mobile-tooltip" style="font-size: 16px; color: black;" class="fas fa-question-circle" data-toggle="tooltip" data-html="true"
-        title="{!! $field->getOption('tooltip') !!}"></i>
-    @endif
 
     <div class="dynamo-group-card card">
 
@@ -16,6 +12,10 @@ $group = $dynamo->getGroup($field->key);
 
             @if ($group->options->has('label'))
                 <h4 style="margin-top: 0px;">{!! $group->options->get('label') !!}</h4>
+            @endif
+            @if ($group->options->has('tooltip'))
+                <i id="dont-show-on-mobile-tooltip" style="font-size: 16px; color: black;" class="fas fa-question-circle" data-toggle="tooltip" data-html="true"
+                title="{!! $field->getOption('tooltip') !!}"></i>
             @endif
 
             {!! $group->render($item) !!}
