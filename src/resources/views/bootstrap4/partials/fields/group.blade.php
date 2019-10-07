71c3d92fd1fa955cafe9750ebeb6e170a29049ab
Colton Williams <clwilliams4545@gmail.com>
2019-10-07 11:35:18 -0500

    Final bug fix 2

--- src/resources/views/bootstrap4/partials/fields/group.blade.php
+++ src/resources/views/bootstrap4/partials/fields/group.blade.php
@@ -11,11 +11,13 @@ $group = $dynamo->getGroup($field->key);
         <div class="card-body">
 
             @if ($group->options->has('label'))
-                <h4 style="margin-top: 0px;">{!! $group->options->get('label') !!}</h4>
-                @if ($group->options->has('tooltip'))
-                    <i id="dont-show-on-mobile-tooltip" style="font-size: 16px; color: black;" class="fas fa-question-circle ml-2" data-toggle="tooltip" data-html="true"
-                    title="{!! $group->options->get('tooltip') !!}"></i>
-                @endif
+                <h4 style="margin-top: 0px;">{!! $group->options->get('label') !!}
+                    @if ($group->options->has('tooltip'))
+                        <i id="dont-show-on-mobile-tooltip" style="font-size: 16px; color: black;" class="fas fa-question-circle ml-2" data-toggle="tooltip" data-html="true"
+                        title="{!! $group->options->get('tooltip') !!}"></i>
+                    @endif
+                </h4>
+                
             @endif
             
 
