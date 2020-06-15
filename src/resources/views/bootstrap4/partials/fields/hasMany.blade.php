f953a06510df7d60476a2cffbd3d40d975dcd731
Colton Williams <clwilliams4545@gmail.com>
2020-06-15 15:57:41 -0500

    Updated partials to move position of help note on dynamo projects on 1.3.*

--- src/resources/views/bootstrap4/partials/fields/hasMany.blade.php
+++ src/resources/views/bootstrap4/partials/fields/hasMany.blade.php
@@ -2,6 +2,9 @@
 
     <div class="form-group">
         <label for="" title="Position: {{ $field->position }}">{{ $field->label }}</label>
+        @if (! empty($field->getOption('help')))
+            <p class="help-block">{!! $field->getOption('help') !!}</p>
+        @endif
         @if (! empty($field->getOption('tooltip')))
             <i style="font-size: 17px; padding-left: 2px;" class="fas fa-question-circle" data-toggle="tooltip" data-html="true"
                 title="{!! $field->getOption('tooltip') !!}"></i>
@@ -15,9 +18,7 @@
                 'class' => 'form-control '.$field->getOption('class')
             ]
         ) !!}
-        @if (! empty($field->getOption('help')))
-            <p class="help-block">{!! $field->getOption('help') !!}</p>
-        @endif
+        
     </div>
 
 </div>
---
+++ src/resources/views/bootstrap4/partials/fields/singleImage.blade.php
@@ -0,0 +1,74 @@
+@if ($display)
+
+    @php
+        $mediaItems = $item->getMedia($field->key);
+    @endphp
+
+    <div class="form-group form-group-gallery form-group-{{ $field->key }}">
+
+        <label for="" title="Position: {{ $field->position }}">
+            {{ $field->label }}
+            @if (! empty($field->getOption('tooltip')))
+                <i id="dont-show-on-mobile-tooltip" style="font-size: 16px; color: black;" class="fas fa-question-circle" data-toggle="tooltip" data-html="true"
+                title="{!! $field->getOption('tooltip') !!}"></i>
+            @endif
+        </label>
+        @if (! empty($field->getOption('help')))
+            <div class="help-block" style="margin-top: -7px;">{!! $field->getOption('help') !!}</div>
+        @endif
+        
+        <gallery-manager
+            :name="'{{ $field->key }}'"
+            :media="{{ $mediaItems->toJson() }}"
+            :model_id="{{ empty($item->id) ? 0 : $item->id }}"
+            :model_type="'{{ addslashes(get_class($item)) }}'"
+
+            @if ($field->getOption('strict'))
+                :strict="{{ $field->getOption('strict') }}"
+            @endif
+
+            @if ($field->getOption('checkOrientation'))
+                :check-orientation="{{ $field->getOption('checkOrientation') }}"
+            @endif
+
+            @if ($field->getOption('maxWidth'))
+                :max-width="{{ $field->getOption('maxWidth') }}"
+            @endif
+
+            @if ($field->getOption('maxHeight'))
+                :max-height="{{ $field->getOption('maxHeight') }}"
+            @endif
+
+            @if ($field->getOption('minWidth'))
+                :min-width="{{ $field->getOption('minWidth') }}"
+            @endif
+
+            @if ($field->getOption('minHeight'))
+                :min-height="{{ $field->getOption('minHeight') }}"
+            @endif
+
+            @if ($field->getOption('width'))
+                :width="{{ $field->getOption('width') }}"
+            @endif
+
+            @if ($field->getOption('height'))
+                :height="{{ $field->getOption('height') }}"
+            @endif
+
+            @if ($field->getOption('quality'))
+                :quality="{{ $field->getOption('quality') }}"
+            @endif
+
+            @if ($field->getOption('mimeType'))
+                :mime-type="{{ $field->getOption('mimeType') }}"
+            @endif
+
+            @if ($field->getOption('convertSize'))
+                :convert-size="{{ $field->getOption('convertSize') }}"
+            @endif
+
+        ></gallery-manager>
+
+    </div>
+
+@endif
